4801c85a7fd7d6803fe9434b20a224a1f947164d
Martin Atanasov <mvatanasov18@codingburgas.bg>
2023-01-07 14:36:24 +0200

    Create PrincipalRepository

---
+++ src/main/java/com/example/students/repositories/PrincipalRepository.java
@@ -0,0 +1,8 @@
+package com.example.students.repositories;
+
+import com.example.students.models.Principal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrincipalRepository extends JpaRepository<Principal, Integer> {
+
+}
